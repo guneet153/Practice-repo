ec26bfac09d9c56c8c64c9486ddcfb2032e78e76
guneet153 <guneetk1811@gmail.com>
2025-12-05 17:04:01 +0530

    cc

---
+++ patterns/invertedstar.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter number");
+    scanf("%d", &n);
+    int a=n;            //using another variable
+    for(int i=1;i<=n;i++)
+{
+    for(int j=1;j<=a;j++)
+    {
+        printf("*");
+    }
+    a--;
+    printf("\n");
+}
+    return 0;
+}
