26568a2e5303e4a99afca92a6cbb1bcfe1f9def9
guneet153 <guneetk1811@gmail.com>
2025-12-05 16:55:32 +0530

    cc

---
+++ patterns/numbersquare.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter number");
+    scanf("%d", &n);
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= n; j++)
+        {
+            printf("%d", j);
+            //printf("%d",i);  //for 1111
+                               //for 2222
+                               //for 3333
+        }
+        printf("\n");
+    }
+    return 0;
+}
